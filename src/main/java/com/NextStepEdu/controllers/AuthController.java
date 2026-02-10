53363594724d881923c6ad73348e0af3218fbcea
RothanaDev <phornrothana8@gmail.com>
2026-02-10 10:33:57 +0700

    Update AuthController

--- src/main/java/com/NextStepEdu/controllers/AuthController.java
+++ src/main/java/com/NextStepEdu/controllers/AuthController.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
